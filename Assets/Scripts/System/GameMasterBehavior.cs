1441b817c5a722062594cc7b46461bec93dc04d5
THS60717\deaftron <shakedpotato@gmail.com>
2019-04-08 20:23:45 +0900

    GameMasterBehaviourにグリッド線オブジェクトの生成を記述。

--- Assets/Scripts/System/GameMasterBehavior.cs
+++ Assets/Scripts/System/GameMasterBehavior.cs
@@ -11,7 +11,7 @@ public class GameMasterBehavior : MonoBehaviour
     // UI
     public GameObject m_EventObjectPrefab;
     public GameObject m_MenuCanvas;
-
+    public GameObject m_GridLinePrefab;
     public static StageInfo InitiatingStage = new StageInfo(1, 1);
 
     private void Awake()
@@ -21,9 +21,13 @@ public class GameMasterBehavior : MonoBehaviour
         // ステージ情報を書いたテキストファイルの読み込み
         var mapData = MapLoader.LoadMap(InitiatingStage);
 
+        // グリッド線を生成
+        Instantiate(m_GridLinePrefab);
+
         // 世界を作る.
         GameObject starMaker = Instantiate(m_StarMakerPrefab);
         starMaker.GetComponent<StarMaker>().MakeWorld(mapData, Common.CellSize);
+
 
         // Event objectを生成. UIの前に必ず生成!
         Instantiate(m_EventObjectPrefab);
