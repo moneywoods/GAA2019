3aefd4020dbd60fb14ecb690d69451f4b459b55d
THS60717\deaftron <shakedpotato@gmail.com>
2019-04-09 20:12:03 +0900

    StarBaseに名前変更。 StarBaseを各星の基底クラスに設定。

--- Assets/Scripts/Star/LandStarController.cs
+++ Assets/Scripts/Star/LandStarController.cs
@@ -3,7 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class LandStarController : MonoBehaviour
+public class LandStarController : StarBase
 {
     public enum ChildIndex
     {
--- Assets/Scripts/Star/StarBase.cs
+++ Assets/Scripts/Star/StarBase.cs
@@ -1,8 +1,9 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class StarBaseClass : MonoBehaviour
+public class StarBase : MonoBehaviour
 {
     private Vector2Int cellNum = new Vector2Int(-1, -1);
     
@@ -30,21 +31,18 @@ public class StarBaseClass : MonoBehaviour
         return CellNum;
     }
 
-    void CaluculateCellPos()
+    public void CaluculateCellPos()
     {
-        // セル位置を計算。
-        Vector3 vec = transform.position - new Vector3(-1.0f, -1.0f, -1.0f);
-        vec.x = Mathf.Round(vec.x); // 丸める
-        vec.y = Mathf.Round(vec.y);
-        vec.z = Mathf.Round(vec.z);
-    }
-    public static GameObject FindStar()
-    {
-        GameObject starMaker = GameObject.FindGameObjectWithTag(ObjectTag.StarMaker);
-
+        GameObject starMaker = GameObject.FindWithTag(ObjectTag.StarMaker);
         if(starMaker == null)
-            return null;
-        
-        return null;
+        {
+            Debug.Log("StarMaker == null. Function: CalculateCellPos. This is " + gameObject.name);
+        }
+        StarMaker.MapInfo mapInfo = starMaker.GetComponent<StarMaker>().CurrentMapInfo;
+        var offset = mapInfo.DeffaultOffset;
+        // セル位置を計算。
+        Vector3 vec = transform.position - offset;
+        cellNum.x = (int) Math.Round(vec.x, MidpointRounding.AwayFromZero) / (int)mapInfo.CellSize.x;// 丸める
+        cellNum.y = (int) Math.Round(-vec.y, MidpointRounding.AwayFromZero) / (int)mapInfo.CellSize.y;
     }
 }
