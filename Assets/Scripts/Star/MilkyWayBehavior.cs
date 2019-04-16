a105f79392a7e0acebe232b36a456b4adec0f1f0
THS60717\deaftron <shakedpotato@gmail.com>
2019-04-16 15:50:45 +0900

    StarBaseのサブクラスへ変更した.

--- Assets/Scripts/Star/MilkyWayBehavior.cs
+++ Assets/Scripts/Star/MilkyWayBehavior.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MilkyWayBehavior : MonoBehaviour
+public class MilkyWayBehavior : StarBase
 {
     // Start is called before the first frame update
     void Start()
