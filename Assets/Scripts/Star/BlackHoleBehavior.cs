19608e86f45c3ee68deb98971d751d7296f7b502
THS60717\deaftron <shakedpotato@gmail.com>
2019-04-19 14:30:52 +0900

    マスへの出入り時に呼び出す関数を記述.

--- Assets/Scripts/Star/BlackHoleBehavior.cs
+++ Assets/Scripts/Star/BlackHoleBehavior.cs
@@ -13,21 +13,47 @@ public class BlackHoleBehavior : StarBase
     // Update is called once per frame
     void Update()
     {
-        
+
     }
 
     private void OnTriggerEnter(Collider collision)
     {
-        if( collision.tag == ObjectTag.Land)
-        {
-            LandStarController landScript = collision.GetComponent<LandStarController>();
-            landScript.AddStat(LandStarController.LANDSTAR_STAT.DESTROYED);
-            landScript.RemoveFlag(LandStarController.LANDSTAR_STAT.MOVING);
-        }
+
     }
 
     private void OnTriggerExit(Collider collision)
     {
         
     }
+
+    public override bool CheckKineticPowerCanBeUsed(Vector2Int originCellNum, bool isRight)
+    {
+        return base.CheckKineticPowerCanBeUsed(originCellNum, isRight);
+    }
+
+    public override void TriggerEnterCell(GameObject other)
+    {
+        base.TriggerEnterCell(other);
+    }
+
+    public override void TriggerExitCell(GameObject other)
+    {
+        base.TriggerExitCell(other);
+    }
+
+    public override void TriggerOtherComeToSameCell(GameObject other)
+    {
+        if(other.tag == ObjectTag.Land)
+        {
+            LandStarController landScript = other.GetComponent<LandStarController>();
+            landScript.AddStat(LandStarController.LANDSTAR_STAT.DESTROYED);
+            landScript.RemoveFlag(LandStarController.LANDSTAR_STAT.MOVING);
+        }
+    }
+
+    public override void TriggerOtherLeaveFromSameCell(GameObject other)
+    {
+        base.TriggerOtherLeaveFromSameCell(other);
+    }
+
 }
