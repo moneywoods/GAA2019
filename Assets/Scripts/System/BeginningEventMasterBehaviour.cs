bea8e7fb93115d8240dd4782afec17d7ac979082
THS60717\deaftron <shakedpotato@gmail.com>
2019-04-23 16:25:34 +0900

    とりあえず

---
+++ Assets/Scripts/System/BeginningEventMasterBehaviour.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BeginningEventMasterBehaviour : MonoBehaviour
+{
+    public bool isSceneEnding = false;
+    private float timeElapsed;
+    private float estimatedTimeOfScene = 10.0f;
+
+    void Awake()
+    {
+        
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeElapsed += Time.deltaTime;
+        if(estimatedTimeOfScene < timeElapsed)
+        {
+            isSceneEnding = true;
+        }
+
+        if(isSceneEnding)
+        {
+
+        }
+    }
+}
