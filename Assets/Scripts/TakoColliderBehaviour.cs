aad39d9f31325d20e0fe391a0a5a1d21be2dbc66
THS60717\deaftron <shakedpotato@gmail.com>
2019-04-23 13:58:49 +0900

    手直し

--- Assets/Scripts/TakoColliderBehaviour.cs
+++ Assets/Scripts/TakoColliderBehaviour.cs
@@ -4,6 +4,10 @@ using UnityEngine;
 
 public class TakoColliderBehaviour : MyGameObject
 {
+    public TakoColliderBehaviour() : base(ObjectType.Character)
+    {
+
+    }
     // Start is called before the first frame update
     protected override void Start()
     {
