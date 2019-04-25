ac8f96389f2092f477e34b5bfe27f7fb6a2d804a
kazuaki-miyamoto <3959kazu0520@gmail.com>
2019-04-25 11:57:10 +0900

    テキスト時間表示

---
+++ Assets/Scripts/UI/Text Massige/TextMessnger.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TextMessnger : MonoBehaviour
+{
+    public Text Qtext;      // Text取得
+    int Textflag;           // Textのalpha値管理フラグ
+    float Textalpha;        // Textのalpha値変更
+    public float Timemax;   // 最大表示時間
+    float Timecount;        // 時間のカウント 
+
+    // Use this for initialization
+    void Start()
+    {
+        
+        Textflag = 0;
+        Textalpha = 0.0f;
+        Timecount = 0.0f;
+        Qtext = GetComponentInChildren<Text>();//UIのテキストの取得の仕方
+        Qtext.text = "代替テキスト";//テキストの変更
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // Textの色変更
+        Qtext.color = new Color(0, 0, 0, Textalpha);
+        
+        // Mキーを押してTextflagを1にする
+        if (Input.GetKey(KeyCode.M))
+        {
+            Textflag = 1;
+            Textalpha = 0.0f;
+        }
+
+        // TimemaxよりもTimecountが大きくなった時、Textflagを2にする
+        if (Timemax < Timecount)
+        {
+            Textflag = 2;
+        }
+
+        // Textflagが0以外の時に実行する
+        if (Textflag == 1)
+        {
+            Textalpha += Time.deltaTime;
+        }
+        else if(Textflag == 2)
+        {
+            Textalpha -= Time.deltaTime;
+        }
+
+        // Textalphaが1より大きいときTimecountを増やす
+        if (Textalpha > 1)
+        {
+            Textalpha = 1;
+            Timecount += Time.deltaTime; 
+        }
+
+        // Textalphaが0より小さいときTextalpha、Textflag、Timecountを0にする
+        else if (Textalpha< 0)
+        {
+            Textalpha = 0.0f;
+            Textflag = 0;
+            Timecount = 0.0f;
+        }
+    }
+}
