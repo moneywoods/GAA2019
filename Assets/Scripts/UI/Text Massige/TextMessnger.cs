c0d9ae30ab6a4ddbba6a43340364f6ddd295c205
kazuaki-miyamoto <3959kazu0520@gmail.com>
2019-05-10 15:22:13 +0900

    テキスト修正完了(仮)

--- Assets/Scripts/UI/Text Massige/TextMessnger.cs
+++ Assets/Scripts/UI/Text Massige/TextMessnger.cs
@@ -12,24 +12,41 @@ public class TextMessnger : MonoBehaviour
     private float Textalpha;        // Textのalpha値変更
     RectTransform Area;             // テキストの座標取得
     private float Timecount;        // 時間のカウント
-
-   
-
+    private bool testflag;          // テスト用フラグ
+    float testtime;                 // テスト用動作時間
 
     // Use this for initialization
     void Start()
     {
-        
         MessngerInit();
+        testflag = false;
+        testtime = 0.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 trect = new Vector3(-200.0f, 0.0f, 0.0f);
-        Color tcolor = new Color(255.0f, 0.0f, 0.0f, 1.0f);
-        MessngerUpdate("テキスト", trect, tcolor, 1);
+        if (Input.GetKey(KeyCode.M))
+        {
+            testflag = true;
+        }
+
+        //テスト用
+        if (testflag == true)
+        {
+            Vector3 trect = new Vector3(0.0f, 0.0f, 0.0f);
+            Color tcolor = new Color(255.0f, 0.0f, 0.0f, 1.0f);
+            MessngerUpdate("テキスト", trect, tcolor, 1, true);
+            testtime += Time.deltaTime;
+        if (testtime > 3.0f)
+        {
+            testflag = false;
+            testtime = 0.0f;
+        }
     }
+
+
+}
 
     public void MessngerInit()
     {
@@ -39,52 +56,51 @@ public class TextMessnger : MonoBehaviour
         Timecount = 0.0f;
         Area = GetComponent<RectTransform>();                       // テキストの座標位置取得の仕方 
         Qtext = GetComponentInChildren<Text>();                     // UIのテキストの取得の仕方
-        
     }
 
-    public void MessngerUpdate(string textset, Vector3 rect, Color textcolor, float Timemax)
+    // MessngerUpdate 引数(文字, テキスト表示位置, テキストカラー, 表示時間, on/off)
+    public void MessngerUpdate(string textset, Vector3 rect, Color textcolor, float Timemax, bool Onflag )
     {
         Area.localPosition = new Vector3(rect.x, rect.y, rect.z);                   // テキスト座標の変更
         Qtext.color = new Color(textcolor.r, textcolor.g, textcolor.b, Textalpha);  // Textの色変更
         Qtext.text = textset;                                                       // テキストの変更
 
-        // Mキーを押してTextflagを1にする
-        if (Input.GetKey(KeyCode.M) && Textflag == 0)
-        {
-            Textflag = 1;
-            Textalpha = 0.0f;
-        }
+        // OnflagがtrueかつTextflagが0のときTextflagを1にする
+        if (Onflag && Textflag == 0)
+       {
+           Textflag = 1;
+           Textalpha = 0.0f;
+       }
 
-        // TimemaxよりもTimecountが大きくなった時、Textflagを2にする
-        if (Timemax < Timecount)
-        {
-            Textflag = 2;
-        }
+       // TimemaxよりもTimecountが大きくなった時、Textflagを2にする
+       if (Timemax <= Timecount)
+       {
+           Textflag = 2;
+       }
 
-        // Textflagが0以外の時に実行する
-        if (Textflag == 1)
-        {
-            Textalpha += Time.deltaTime;
-        }
-        else if (Textflag == 2)
-        {
-            Textalpha -= Time.deltaTime;
-        }
+       // Textflagが0以外の時に実行する
+       if (Textflag == 1)
+       {
+           Textalpha += Time.deltaTime;
+       }
+       else if (Textflag == 2)
+       {
+           Textalpha -= Time.deltaTime;
+       }
 
-        // Textalphaが1より大きいときTimecountを増やす
-        if (Textalpha > textcolor.a)
-        {
-            Textalpha = textcolor.a;
-            Timecount += Time.deltaTime;
-        }
-
-        // Textalphaが0より小さいときTextalpha、Textflag、Timecountを0にする
-        else if (Textalpha < 0)
-        {
-            Textalpha = 0.0f;
-            Textflag = 0;
-            Timecount = 0.0f;
-        }
-
+        // Textalphaがtextcolor.aより大きいときTimecountを増やす
+        if (Textalpha >= textcolor.a)
+       {
+           Textalpha = textcolor.a;
+           Timecount += Time.deltaTime;
+       } 
+       // Textalphaが0より小さいときTextalpha、Textflag、Timecountを0にする
+       else if (Textalpha <= 0)
+       {
+           Textalpha = 0.0f;
+           Textflag = 0;
+           Timecount = 0.0f;
+            Onflag = false;
+       }
     }
 }
