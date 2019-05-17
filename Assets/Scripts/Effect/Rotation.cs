b6587605d4005cdc6d7d7f74fd07a1548921854c
Daijiro123 <48054025+Daijiro123@users.noreply.github.com>
2019-05-17 16:05:15 +0900

    オクトの周りをフラフープみたいな動きで回るパーチカルを追加

--- Assets/Scripts/Effect/Rotation.cs
+++ Assets/Scripts/Effect/Rotation.cs
@@ -9,13 +9,13 @@ public class Rotation : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        m_ObjTako = GameObject.FindWithTag("PlayerCharacter");
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(m_ObjTako == null) m_ObjTako = GameObject.FindWithTag("PlayerCharacter");
+
         transform.position = m_ObjTako.transform.position;
-        transform.Rotate(new Vector3(0, 0, 1));
     }
 }
