9ff75c8d0b55e0854fd86cb25d97b9cb7b8065aa
RNY1120 <fiesta11fata@gmail.com>
2019-05-17 15:06:21 +0900

    とりあえず星をばらまく謎Particleの実験途中

---
+++ Assets/Scripts/ParticleScript.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ParticleScript : MonoBehaviour
+{
+    //　出現させるエフェクト
+    [SerializeField]
+    private GameObject effectObject;
+    //　エフェクトを消す秒数
+    [SerializeField]
+    private float deleteTime;
+
+    LandStarController Star;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Star = GetComponent<LandStarController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Star.CheckFlag(LandStarController.LANDSTAR_STAT.MOVING))
+        {
+            var instantiateEffect = GameObject.Instantiate(effectObject, transform.position + new Vector3(0f, 0f, 0f), Quaternion.identity) as GameObject;
+            Destroy(instantiateEffect, deleteTime);
+        }
+    }
+}
