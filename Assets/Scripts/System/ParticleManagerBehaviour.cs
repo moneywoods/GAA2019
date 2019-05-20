9b357e7b56c7254041d97b26f45a807eb02246ab
shakedpotato <shakedpotato@gmail.com>
2019-05-20 12:40:19 +0900

    ParticleManager実装

---
+++ Assets/Scripts/System/ParticleManagerBehaviour.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+#if UNITY_EDITOR
+using UnityEditor;
+#endif // UNITY_EDITOR
+
+public class ParticleManagerBehaviour : SingletonPattern<ParticleManagerBehaviour>
+{
+    public enum ParticleIndex
+    {
+        TEST
+    }
+
+    class MyParticle
+    {
+        public GameObject ParticleObj;
+        public ParticleIndex index;
+        public MyParticle(GameObject part, ParticleIndex index)
+        {
+            this.ParticleObj = part;
+            this.index = index;
+        }
+    }
+
+    List<MyParticle> ParticlePrefabList;
+
+
+    // Prefab置き場
+    [SerializeField] GameObject test;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ParticlePrefabList = new List<MyParticle>();
+        ParticlePrefabList.Add(new MyParticle(test, ParticleIndex.TEST));
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public GameObject GetParticle(ParticleIndex index)
+    {
+        return ParticlePrefabList.Find(obj => obj.index == index).ParticleObj;
+    }
+
+
+}
