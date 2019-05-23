b1895da1172e2552720cb62d677c44ba8a834f42
Daijiro123 <48054025+Daijiro123@users.noreply.github.com>
2019-05-23 11:17:11 +0900

    爆発した時のパーチクルを追加。動作も確認済み。 パーチクル自身の編集点があればParticleSystemをいじります。

--- Assets/Scripts/Star/LandStarController.cs
+++ Assets/Scripts/Star/LandStarController.cs
@@ -52,10 +52,6 @@ public class LandStarController : StarBase
         set;
     }
 
-    // 移住可能を示すエフェクト // 今後UIとかもっと他の物に置き換える予定
-    public GameObject m_EffectCanMoveTo;
-    protected bool m_isCanMoveToEffectEmitting;
-
     public LandStarController() : base(StarType.Land)
     {
         
@@ -65,7 +61,6 @@ public class LandStarController : StarBase
     protected override void Start()
     {
         timePast = 0.0f;
-        m_isCanMoveToEffectEmitting = false;
     }
 
     // Update is called once per frame
