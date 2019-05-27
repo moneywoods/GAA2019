547a6d4cb16ae9ef1f3160bf20c12ed84832f551
KAPPAOFFLINE <kappaoffline@gmail.com>
2019-05-27 21:19:13 +0900

    カメラワークに必要なアセットの導入

---
+++ Assets/LayerName.cs
@@ -0,0 +1,24 @@
+﻿/// <summary>
+/// レイヤー名を定数で管理するクラス
+/// </summary>
+public static class LayerName
+{
+	public const int Default = 0;
+	public const int TransparentFX = 1;
+	public const int IgnoreRaycast = 2;
+	public const int Water = 4;
+	public const int UI = 5;
+	public const int Player = 8;
+	public const int Star = 9;
+	public const int CellCollider = 10;
+	public const int Grid = 11;
+	public const int DefaultMask = 1;
+	public const int TransparentFXMask = 2;
+	public const int IgnoreRaycastMask = 4;
+	public const int WaterMask = 16;
+	public const int UIMask = 32;
+	public const int PlayerMask = 256;
+	public const int StarMask = 512;
+	public const int CellColliderMask = 1024;
+	public const int GridMask = 2048;
+}
