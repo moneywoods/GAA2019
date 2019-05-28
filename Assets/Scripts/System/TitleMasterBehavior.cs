8275d4dee308b2651f1f17a34369a7c62244601d
shakedpotato <shakedpotato@gmail.com>
2019-05-28 16:29:14 +0900

    使われていない項目を削除

--- Assets/Scripts/System/TitleMasterBehavior.cs
+++ Assets/Scripts/System/TitleMasterBehavior.cs
@@ -7,8 +7,6 @@ using UnityEngine.UI;
 public class TitleMasterBehavior : MonoBehaviour
 {
     public GameObject m_ParentCanvasPrefab;
-    public GameObject m_EventObjectPrefab;
-
 
     // Start is called before the first frame update
     void Start()
@@ -16,19 +14,8 @@ public class TitleMasterBehavior : MonoBehaviour
         // フェードイン
         FadeManager.FadeIn();
 
-        // Event objectを生成. UIの前に必ず生成!
-        Instantiate(m_EventObjectPrefab);
-
         // UI objectを生成.
         GameObject menu = Instantiate(m_ParentCanvasPrefab);
         menu.GetComponent<ParentMenuCanvasBehavior>().SetActivateSelectionCursor();
     }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
-
-
 }
