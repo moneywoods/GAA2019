58ec87ada9b6a1f2799ed9b99687e0fad82b667c
Daijiro123 <48054025+Daijiro123@users.noreply.github.com>
2019-05-28 16:25:50 +0900

    BeginningEventのOctoが上から舞い降りてくるシーンを作成。

---
+++ Assets/Scripts/BeginningEventScene/VCam.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class VCam : MonoBehaviour
+{
+    private ICinemachineCamera m_VCam;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        GameObject objOcto = GameObject.FindWithTag("PlayerCharacter");
+        m_VCam = GetComponent<ICinemachineCamera>();
+
+        m_VCam.Follow = objOcto.transform;
+        m_VCam.LookAt = objOcto.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+    }
+}
--- Assets/Scripts/System/BeginningEventMasterBehaviour.cs
+++ Assets/Scripts/System/BeginningEventMasterBehaviour.cs
@@ -1,12 +1,25 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class BeginningEventMasterBehaviour : MonoBehaviour
 {
     public bool isSceneEnding = false;
-    private float timeElapsed;
-    private float estimatedTimeOfScene = 3.0f;
+
+    [SerializeField]
+    private GameObject m_Octo;
+    [SerializeField]
+    private GameObject m_VirtualCamera;
+
+    [SerializeField]
+    private float m_Speed;
+
+    [SerializeField]
+    private  float TIMELIMIT;
+
+    private OctoStartMove m_OctoScript;
+    private float m_Timer;
 
     void Awake()
     {
@@ -15,21 +28,54 @@ public class BeginningEventMasterBehaviour : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        if(GameMasterBehavior.InitiatingStage.Stage == 1 || GameMasterBehavior.InitiatingStage.Stage == 0)
+        {
+            // 背景用のシーン読込
+            SceneManager.LoadScene("GameBackGround", LoadSceneMode.Additive);
+        }
+
+        // ゲームオブジェクト生成
+        m_Octo.transform.position = new Vector3(0f, 0f, 0f);
+        Instantiate(m_Octo, m_Octo.transform);
+        Instantiate(m_VirtualCamera);
+
+        // 生成されたオクトのクローンのスクリプト
+        m_OctoScript = GameObject.FindWithTag("PlayerCharacter").GetComponent<OctoStartMove>();
     }
 
     // Update is called once per frame
     void Update()
     {
-        timeElapsed += Time.deltaTime;
-        if(estimatedTimeOfScene < timeElapsed)
+        OctoAdmissionScene();
+    }
+
+    private void OctoAdmissionScene()
+    {
+        if (IsMove())
         {
             isSceneEnding = true;
+        }else
+        {
+            m_OctoScript.OctoAdmission(m_Speed);
         }
 
-        if(isSceneEnding)
+        if (isSceneEnding)
+        {
+            m_Timer += Time.deltaTime;
+
+        }
+        if (TIMELIMIT < m_Timer)
         {
             FadeManager.FadeOut("scene0315");
         }
     }
+
+
+    private bool IsMove()
+    {
+        Vector3 octoPos = m_OctoScript.GetPos();
+
+        return octoPos.y <= 5f;
+    }
+    
 }
