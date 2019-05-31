0e8c7c8502b8f76c4e476f18e2b4faf632a1bfdd
RNY1120 <fiesta11fata@gmail.com>
2019-05-31 15:44:20 +0900

    クラゲが捕まえないけどコミット

---
+++ Assets/Scripts/Star/JellyAnime.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyAnime : MonoBehaviour
+{
+
+    private Animator animator;
+    LandStarController Script;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Script = GetComponent<LandStarController>();
+
+        animator = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+        //障害物につかまってる時
+        if (Script.CheckFlag(LandStarController.LANDSTAR_STAT.STUCKED))
+        {
+            animator.SetBool("is_catch", true);
+
+        }
+
+        else
+        {
+            animator.SetBool("is_catch", false);
+        }
+    }
+}
