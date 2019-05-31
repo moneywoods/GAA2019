84b8f0e408903104425c50adbddf732d84377791
Daijiro123 <48054025+Daijiro123@users.noreply.github.com>
2019-05-31 18:13:41 +0900

    クラゲのアニメーション用に作成

---
+++ Assets/Scripts/Star/JellyfishAnim.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JellyfishAnim : MonoBehaviour
+{
+    private Animator animator;
+    private GameObject JellyfishModel;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        JellyfishModel = gameObject;
+        animator = JellyfishModel.GetComponent<Animator>();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void PlayAnim()
+    {
+    }
+}
